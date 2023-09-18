ca6e09d829856e9e53320e8e87ceb5478ecac5dc
Sara Lorrayne <saralorrayne@hotmail.com>
2023-09-18 14:33:08 -0300

    git commit

---
+++ server/index.js
@@ -0,0 +1,11 @@
+import cors from "cors"
+import express from "express"
+
+const app = express()
+app.use(cors())
+
+app.get("/summary/:id", (request, response) => {
+  response.send("Id do vídeo:" + request.params.id)
+})
+
+app.listen(3333, () => console.log("Server is running on port 3333"))
