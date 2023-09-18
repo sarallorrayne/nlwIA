ef7962bc55364117e7b90bf34e122881af677c47
Sara Lorrayne <saralorrayne@hotmail.com>
2023-09-18 14:35:09 -0300

    git commit

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 import cors from "cors"
 import express from "express"
 
+import { download } from "./download.js"
+import { transcribe } from "./transcribe.js"
+
 const app = express()
 app.use(cors())
 
-app.get("/summary/:id", (request, response) => {
-  response.send("Id do vídeo:" + request.params.id)
+app.get("/summary/:id", async (request, response) => {
+  await download(request.params.id)
+  const result = transcribe()
+
+  response.json ({ result })
 })
 
 app.listen(3333, () => console.log("Server is running on port 3333"))
